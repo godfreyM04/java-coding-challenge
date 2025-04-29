b7a1bccf860c74f8d6951c3c2e756683ad5529db
godfreyM04 <gomark026@gmail.com>
2025-04-29 09:26:21 +0300

    Finished If else application coding challenge

---
+++ IfElse.java
@@ -0,0 +1,10 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int num = 4;
+        if(num > 5) {
+            System.out.println("Number is greater than 5");
+        } else {
+            System.out.println("Number is less than 5");
+        }
+    }
+}
